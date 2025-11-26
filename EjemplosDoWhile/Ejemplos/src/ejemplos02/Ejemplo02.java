44a6e9a7215810206132c28c72597b8ad9f6241b
svalverde007 <sdvalverde2@utpl.edu.ec>
2025-11-26 11:54:49 -0500

    solución problema 2

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -25,18 +25,19 @@ public class Ejemplo02 {
         double nota;
         boolean bandera = true;
         String salida;
+
         do {
             System.out.println("Ingrese calificación");
             nota = entrada.nextDouble();
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (s, si, yes, y) si desea salir del ciclo ");
             salida = entrada.nextLine();
-
-            
-            
-
+            if (salida.equals("s") || salida.equals("si")
+                    || salida.equals("y")){
+            bandera = false;
+            }
         } while (bandera); // (bandera==true)
 
         System.out.printf("Listado de Notas\n%s\n", cadenaFinal);
